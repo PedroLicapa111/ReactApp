15370203bfecc43bac1098fba67a919b37cfd886
Pedro <pedro.licapa111@gmail.com>
2023-11-10 11:56:50 -0500

    v1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Instituto Superior Honorio Delgado Espinoza - Pedro Licapa Inga
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Turno Mañana
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
